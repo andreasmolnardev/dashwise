de3cf476e8e9455298d0663296815e07729ab9d7
Andreas Molnar <andreas24juni@gmail.com>
2025-10-18 23:07:48 +0200

    redesigned general settings page

--- app/(config-wrapper)/settings/general/page.tsx
+++ app/(config-wrapper)/settings/general/page.tsx
@@ -7,8 +7,8 @@ export default function GeneralSettingsPage() {
         <h2 className="text-xl font-semibold col-span-full">App Info</h2>
         <div className="flex items-center justify-center gap-5"> <img src="/dashwise-icon.png" className="h-14"/> <span><span className="font-semibold text-center text-2xl">dashwise</span> <br /> Version {config.version}</span></div>
         <ul className="col-span-full flex gap-2 justify-center my-2">
-          <li className="frosted rounded-md px-2 py-1 font-medium min-w-40 text-center"><a href="https://github.com/andreasmolnardev/dashwise-next" className="hover:text-(--primary)">Github Repo</a></li>
-          <li className="frosted rounded-md px-2 py-1 font-medium min-w-40 text-center"><a href="https://github.com/andreasmolnardev/dashwise-next/issues" className="hover:text-(--primary)">Github Issues</a></li>
+          <li className="frosted rounded-md px-2 py-1 font-medium min-w-40 text-center"><a href="https://github.com/andreasmolnardev/dashwise-next" className="hover:text-(--primary)">GitHub Repo</a></li>
+          <li className="frosted rounded-md px-2 py-1 font-medium min-w-40 text-center"><a href="https://github.com/andreasmolnardev/dashwise-next/issues" className="hover:text-(--primary)">GitHub Issues</a></li>
         </ul>
      </div></>;
 }
