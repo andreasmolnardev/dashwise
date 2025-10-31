e258bfd5e4df3e41d9b9059eb9944971acfacb5e
Andreas Molnar <andreas24juni@gmail.com>
2025-10-31 21:44:05 +0100

    jobs: only require pocketbase connection vars

--- jobs/src/config/env.ts
+++ jobs/src/config/env.ts
@@ -3,17 +3,24 @@ dotenv.config();
 
 export const config = {
   PB_URL: process.env.PB_URL!,
-  SEARCHITEMS_SCHEDULE: process.env.SEARCHITEMS_SCHEDULE!,
-  MONITORING_INDEXER_SCHEDULE: process.env.MONITORING_INDEXER_SCHEDULE!,
-  MONITORING_RUNNER_SCHEDULE: process.env.MONITORING_RUNNER_SCHEDULE!,
-  ALLOW_SSL: process.env.ALLOW_SSL!,
+  SEARCHITEMS_SCHEDULE: process.env.SEARCHITEMS_SCHEDULE || "*/10 * * * *",
+  MONITORING_INDEXER_SCHEDULE: process.env.MONITORING_INDEXER_SCHEDULE || "*/10 * * * *",
+  MONITORING_RUNNER_SCHEDULE: process.env.MONITORING_RUNNER_SCHEDULE || "*/1 * * * *",
+  ALLOW_SSL: process.env.ALLOW_SSL === "true",
   PB_ADMIN_EMAIL: process.env.PB_ADMIN_EMAIL!,
   PB_ADMIN_PASSWORD: process.env.PB_ADMIN_PASSWORD!,
-  DASHWISE_URL: process.env.DASHWISE_URL
+  DASHWISE_URL: process.env.DASHWISE_URL || undefined
 } as const;
 
 export type Config = typeof config;
 
-for (const [key, value] of Object.entries(config)) {
-  if (!value) throw new Error(`Missing environment variable: ${key}`);
+// Only check for the *required* ones
+const requiredKeys: (keyof Config)[] = [
+  "PB_URL",
+  "PB_ADMIN_EMAIL",
+  "PB_ADMIN_PASSWORD",
+];
+
+for (const key of requiredKeys) {
+  if (!config[key]) throw new Error(`Missing required environment variable: ${key}`);
 }
