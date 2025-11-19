1a7af2db837b3a6fc24e1a256ed7361247198bc0
Andreas Molnar <andreas24juni@gmail.com>
2025-11-19 18:05:14 +0100

    add cloud to weather icons

--- components/widgets/dashboard/Weather.tsx
+++ components/widgets/dashboard/Weather.tsx
@@ -99,7 +99,7 @@ export function getWeatherIcon({ description = "", iconUrl, weatherCode, size = 
 
   if (night === true || nightVersion === true) {
     if (file.includes("sun-clear")) file = "moon-stars-night.svg";
-    if (file.includes("cloudy-sun")) file = "clouds-100.svg";
+    if (file.includes("cloudy-sun")) file = "cloud.svg";
   }
 
   if (file == "moon-stars-night.svg") {
