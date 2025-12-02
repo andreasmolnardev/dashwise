266e29b723a207e46535815240e4fd85acb64329
Andreas Molnar <andreas24juni@gmail.com>
2025-12-02 18:36:25 +0100

    signup page: check if user signup is disabled

--- app/(auth)/auth/signup/page.tsx
+++ app/(auth)/auth/signup/page.tsx
@@ -1,7 +1,10 @@
 import SignupCard from "@/components/auth/SignupForm";
 import config from "@/lib/config";
 
-export default function LoginPage() {
+export default function SignupPage() {
+    if (config.disableUserSignup) {
+        return (<div className="flex items-center justify-center min-h-screen">Signup has been disabled by the admin</div>)
+    }
     return (
         <div
             style={{ backgroundImage: `url(${config.default_bg_url})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
