5b575c408eed0ebc9338fcfa1eea3f8423ad143f
Andreas Molnar <andreas24juni@gmail.com>
2025-12-20 17:12:19 +0100

    auto set job webhook enable var if url is set

--- lib/config.ts
+++ lib/config.ts
@@ -2,6 +2,7 @@ interface Config {
   app_base_url: string;
   pb_url: string;
   jobs_url?: string | undefined;
+  jobs_webhook_enabled: boolean;
   default_bg_url: string;
   version: string;
   allowInsecureCertsForIntegrationUrls: boolean;
@@ -20,9 +21,13 @@ const enableSSOLogin =
 const disableUserSignup =
   process.env.NEXT_PUBLIC_DISABLE_USER_SIGNUP === 'true' || process.env.NEXT_PUBLIC_DISABLE_USER_SIGNUP === '1' || false;
 
+const enableJobsWebhook =
+  process.env.NEXT_PUBLIC_JOBS_WEBHOOK_ENABLE  === 'true' || process.env.NEXT_PUBLIC_JOBS_WEBHOOK_ENABLE === '1' || !!process.env.NEXT_PUBLIC_JOBS_URL || false;
+
 const config: Config = {
   app_base_url: process.env.NEXT_PUBLIC_APP_URL || 'http://dashwise:3000',
   pb_url: process.env.NEXT_PUBLIC_PB_URL || 'http://127.0.0.1:8090',
+  jobs_webhook_enabled: enableJobsWebhook,
   jobs_url: process.env.NEXT_PUBLIC_JOBS_URL || 'http://127.0.0.1:3001',
   default_bg_url: process.env.NEXT_PUBLIC_DEFAULT_BG_URL || '/dashboard-wallpaper.png',
   version: '0.3.1',
