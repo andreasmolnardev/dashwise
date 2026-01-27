71c29f22e3e7adb0efb0144cfc041e3f81ce3fcf
Andreas Molnar <andreas24juni@gmail.com>
2026-01-27 20:35:56 +0100

    swap buttons in subscription details form

--- components/news/SubscriptionDetailsForm.tsx
+++ components/news/SubscriptionDetailsForm.tsx
@@ -183,7 +183,15 @@ export default function SubscriptionDetailsForm({
       </div>
 
       <div className="pt-2">
-        <div className="flex gap-3">
+        <div className="flex gap-3 justify-end">
+          <Button
+            type="button"
+            variant="outline"
+            onClick={onClose}
+            disabled={loading}
+          >
+            Cancel
+          </Button>
           <Button type="submit" disabled={loading}>
             {loading
               ? isEditing
@@ -193,14 +201,7 @@ export default function SubscriptionDetailsForm({
                 ? "Save"
                 : "Subscribe"}
           </Button>
-          <Button
-            type="button"
-            variant="outline"
-            onClick={onClose}
-            disabled={loading}
-          >
-            Cancel
-          </Button>
+
         </div>
       </div>
 
