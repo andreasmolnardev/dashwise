75ace7fff1b97fc064d896e202fd73e511b43a1a
Andreas Molnar <andreas24juni@gmail.com>
2026-03-11 15:28:19 +0100

    use dashwise sdk from file path

--- lib/sdk.ts
+++ lib/sdk.ts
@@ -1,6 +1,6 @@
 import config from "./config";
-import { DashwiseSDKConnector } from "@dashwise/sdk";
-import { registerDashwiseSDKConnector } from "@dashwise/sdk/lib/pocketbase";
+import { DashwiseSDKConnector } from "../dashwise-sdk";
+import { registerDashwiseSDKConnector } from "../dashwise-sdk/lib/pocketbase";
 
 export const dashwiseSDK = new DashwiseSDKConnector({
   pbUrl: config.pb_url,
