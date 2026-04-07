dea68f76ef72a1d90d5a6fc1d398ec7b4efef58b
Andreas Molnar <andreas24juni@gmail.com>
2026-04-07 18:41:41 +0200

    remove news onboarding page

--- apps/web/src/app/(authenticated)/apps/news/onboarding/page.tsx
+++
@@ -1,9 +0,0 @@
-import NewsSubscriptionsOverview from "@/components/news/Subscriptions";
-
-export default function NewsPage() {
-     return (
-        <div className="flex flex-col h-dvh bg-(--surface) backdrop-blur-[5px] backdrop-brightness-85 text-white p-8">
-            <NewsSubscriptionsOverview />
-        </div>
-    );
-}
