e6f417da7ee110f004dfc84e4e89c929396c599b
Andreas Molnar <andreas24juni@gmail.com>
2026-05-24 18:14:19 +0200

    remove userSearchItems table, as it is deprecated now

---
+++ pocketbase/migrations/1779638135_deleted_userSearchItems.js
@@ -0,0 +1,84 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_169670634");
+
+  return app.delete(collection);
+}, (app) => {
+  const collection = new Collection({
+    "createRule": null,
+    "deleteRule": null,
+    "fields": [
+      {
+        "autogeneratePattern": "[a-z0-9]{15}",
+        "help": "",
+        "hidden": false,
+        "id": "text3208210256",
+        "max": 15,
+        "min": 15,
+        "name": "id",
+        "pattern": "^[a-z0-9]+$",
+        "presentable": false,
+        "primaryKey": true,
+        "required": true,
+        "system": true,
+        "type": "text"
+      },
+      {
+        "autogeneratePattern": "",
+        "help": "",
+        "hidden": false,
+        "id": "text1689669068",
+        "max": 0,
+        "min": 0,
+        "name": "associatedUserId",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": false,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "help": "",
+        "hidden": false,
+        "id": "json652256059",
+        "maxSize": 0,
+        "name": "searchItems",
+        "presentable": false,
+        "required": false,
+        "system": false,
+        "type": "json"
+      },
+      {
+        "hidden": false,
+        "id": "autodate2990389176",
+        "name": "created",
+        "onCreate": true,
+        "onUpdate": false,
+        "presentable": false,
+        "system": false,
+        "type": "autodate"
+      },
+      {
+        "hidden": false,
+        "id": "autodate3332085495",
+        "name": "updated",
+        "onCreate": true,
+        "onUpdate": true,
+        "presentable": false,
+        "system": false,
+        "type": "autodate"
+      }
+    ],
+    "id": "pbc_169670634",
+    "indexes": [],
+    "listRule": "@request.auth.id = associatedUserId",
+    "name": "userSearchItems",
+    "system": false,
+    "type": "base",
+    "updateRule": null,
+    "viewRule": "@request.auth.id = associatedUserId"
+  });
+
+  return app.save(collection);
+})
