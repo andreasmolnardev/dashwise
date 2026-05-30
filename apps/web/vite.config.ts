612375d01e2a102bc37b3c2b533babe6d9b6246a
Andreas Molnar <andreas24juni@gmail.com>
2026-05-30 23:12:57 +0200

    update dependencies

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -8,15 +8,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "react": path.resolve(__dirname, "../../node_modules/react"),
-      "react-dom": path.resolve(__dirname, "../../node_modules/react-dom"),
-      "react-dom/client": path.resolve(__dirname, "../../node_modules/react-dom/client"),
-      "react-dom/server": path.resolve(__dirname, "../../node_modules/react-dom/server"),
-      "react-dom/server.browser": path.resolve(__dirname, "../../node_modules/react-dom/server.browser"),
-      "react-dom/server.bun": path.resolve(__dirname, "../../node_modules/react-dom/server.bun"),
-      "react-dom/server.node": path.resolve(__dirname, "../../node_modules/react-dom/server.node"),
-      "react/jsx-runtime": path.resolve(__dirname, "../../node_modules/react/jsx-runtime"),
-      "react/jsx-dev-runtime": path.resolve(__dirname, "../../node_modules/react/jsx-dev-runtime"),
     },
     dedupe: ["react", "react-dom", "scheduler"],
   },
